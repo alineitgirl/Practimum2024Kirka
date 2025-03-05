d9b5f0a9c9bec3b05d1a38b3f276a2d758171f41
alineitgirl <queenalina2005@gmail.com>
2025-03-05 21:46:58 +0300

    feature: Add grpc client-server weather service

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using WeatherClient;
+
+var httpHandler = new HttpClientHandler();
+            
+httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7276", new GrpcChannelOptions
+{
+    HttpHandler = httpHandler
+});
+
+var client = new WeatherService.WeatherServiceClient(channel);
+
+var currentWeather = await client.GetCurrentWeatherAsync(
+    new WeatherRequest { City = "Москва" });
+
+Console.WriteLine($"Текущая погода: {currentWeather.City}, " +
+                  $"{currentWeather.Temperature}°C, {currentWeather.Description}");
+
+
+Console.WriteLine("Начинаем мониторинг погоды...");
+using var monitoring = client.MonitorWeather(new WeatherRequest { City = "Москва" });
+
+try
+{
+    await foreach (var weather in monitoring.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine($"Обновление: {weather.City}, " +
+                          $"{weather.Temperature}°C, {weather.Timestamp}");
+    }
+}
+catch (RpcException ex)
+{
+    Console.WriteLine($"Ошибка: {ex.Message}");
+}
---
+++ GrpcServer/Program.cs
@@ -0,0 +1,21 @@
+using GrpcServer.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(7276, listenOptions =>
+    {
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core
+            .HttpProtocols.Http1AndHttp2;
+        listenOptions.UseHttps();
+    });
+});
+
+builder.Services.AddGrpc();
+builder.Services.AddSingleton<WeatherServiceImpl>();
+
+var app = builder.Build();
+
+app.MapGrpcService<WeatherServiceImpl>();
+app.Run();
